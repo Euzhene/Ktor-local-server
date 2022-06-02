6891926f67bc7031dccbd30f43d218bb7c38c40b
Zhenya20062 <matveeveech1986@yandex.ru>
2022-06-02 19:20:50 +0300

    get chatUser; if there's more than one users with the same login - throws the exception

--- src/main/kotlin/euzhene_chat/room/RoomController.kt
+++ src/main/kotlin/euzhene_chat/room/RoomController.kt
@@ -1,29 +1,34 @@
 package euzhene_chat.room
 
+import euzhene_chat.data.ChatUserDataSource
 import euzhene_chat.data.MessageDataSource
+import euzhene_chat.data.model.ChatUser
 import euzhene_chat.data.model.Message
+import euzhene_chat.data.model.UserInputData
 import io.ktor.http.cio.websocket.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 class RoomController(
-    private val messageDataSource: MessageDataSource
+    private val messageDataSource: MessageDataSource,
+    private val chatUserDataSource: ChatUserDataSource,
 ) {
     private val members = ConcurrentHashMap<String, Member>()
 
-    fun onJoin( //add a member to members list to store his data on our server in order to have easy access to them
-        username: String,
+    suspend fun onJoin( //add a member to members list to store his data on our server in order to have easy access to them
+        userInputData: UserInputData,
         sessionId: String,
         socket: WebSocketSession
     ) {
-        if (members.containsKey(username)) {
-            throw MemberAlreadyExistsException()
-        }
-        members[username] = Member(
-            username, sessionId, socket
+        val chatUser = findUserInDb(userInputData)
+        findMemberInDb(
+            chatUser.login,
+            chatUser.username,
+            sessionId,
+            socket
         )
+
     }
 
     suspend fun sendMessages(username: String, text: String) {
@@ -36,13 +41,33 @@ class RoomController(
             member.socket.send(Frame.Text(parsedMessage))
         }
     }
-    suspend fun getMessages():List<Message> {
+
+    suspend fun getMessages(): List<Message> {
         return messageDataSource.getMessages()
     }
+
     suspend fun tryDisconnect(username: String) {
         members[username]?.socket?.close()
         if (members.containsKey(username)) {
             members.remove(username)
         }
+    }
+
+    private fun findMemberInDb(
+        login: String,
+        username: String,
+        sessionId: String,
+        socket: WebSocketSession
+    ) {
+        if (members.containsKey(login)) {
+            throw MemberAlreadyExistsException()
+        }
+        members[username] = Member(
+            login, username, sessionId, socket
+        )
+    }
+
+    private suspend fun findUserInDb(userInputData: UserInputData): ChatUser {
+        return chatUserDataSource.getUser(userInputData) ?: throw UserNotExistsException()
     }
 }
